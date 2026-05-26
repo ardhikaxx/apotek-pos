2dfe978f6c931e7cfdb8e46c372a77d38a48f371
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-26 22:24:05 +0700

    feat: penyesuaian warna ikon pada bagian keunggulan layanan pelanggan

--- resources/views/pelanggan/products/index.blade.php
+++ resources/views/pelanggan/products/index.blade.php
@@ -21,7 +21,7 @@
     <div class="col-md-4">
         <div class="card border-0 shadow-sm p-3">
             <div class="card-body d-flex align-items-center gap-3">
-                <div class="bg-success bg-opacity-10 text-success p-3 rounded-circle">
+                <div class="bg-info bg-opacity-10 text-info p-3 rounded-circle">
                     <i class="fa fa-tags fs-4"></i>
                 </div>
                 <div>
@@ -34,7 +34,7 @@
     <div class="col-md-4">
         <div class="card border-0 shadow-sm p-3">
             <div class="card-body d-flex align-items-center gap-3">
-                <div class="bg-primary bg-opacity-10 text-primary p-3 rounded-circle">
+                <div class="bg-info bg-opacity-10 text-info p-3 rounded-circle">
                     <i class="fa fa-user-doctor fs-4"></i>
                 </div>
                 <div>
